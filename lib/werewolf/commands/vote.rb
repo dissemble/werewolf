18d83d3b9b3935e2accf23941b657f3b15ce0ae4
swp <seth.plough+github@gmail.com>
2016-09-27 23:38:02 -0400

    move communication logic to game class

---
+++ lib/werewolf/commands/vote.rb
@@ -0,0 +1,20 @@
+module Werewolf
+  module Commands
+    class Vote < SlackRubyBot::Commands::Base
+      command 'vote' do |client, data, match|
+        puts '**** vote ****'
+        puts "client: #{client}"
+        puts "data: #{data}"
+        puts "data.user: #{data.user}"
+        puts "match: #{match}"
+        puts "match['bot']:  #{match['bot']}"
+        puts "match['command']:  #{match['command']}"
+        puts "match['expression']:  #{match['expression']}"        
+        puts '........'
+
+        game = Game.instance
+        game.process_vote(data.user, match['expression'], client, data.channel)
+      end
+    end
+  end
+end
