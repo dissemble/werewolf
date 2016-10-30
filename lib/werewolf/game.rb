f214da2a36448b3e807a8b1aab352bf6ab570e39
swp <seth.plough+github@gmail.com>
2016-10-29 20:14:11 -0400

    more roleset tweaking

--- lib/werewolf/game.rb
+++ lib/werewolf/game.rb
@@ -357,7 +357,7 @@ module Werewolf
         3 => ['seer', 'villager', 'wolf'],
         4 => ['seer', 'villager', 'villager', 'wolf'],
         5 => ['seer', 'bodyguard', 'villager', 'wolf', 'wolf'],
-        6 => ['seer', 'bodyguard', 'villager', 'villager', 'wolf', 'wolf'],
+        6 => ['seer', 'bodyguard', 'beholder', 'villager', 'wolf', 'wolf'],
         7 => ['seer', 'bodyguard', 'beholder', 'villager', 'cultist', 'wolf', 'wolf'],
         8 => ['seer', 'bodyguard', 'beholder', 'villager', 'villager', 'cultist', 'wolf', 'wolf'],
         9 => ['seer', 'bodyguard', 'beholder', 'villager', 'villager', 'villager', 'cultist', 'wolf', 'wolf'],
