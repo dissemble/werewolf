3d5db762aeed3b1699b1c4b60537a53c2054ee61
Daniel Ng <ngenator@gmail.com>
2016-11-01 23:36:53 -0400

    Adding a class to store the game state

---
+++ lib/werewolf/engine/state.rb
@@ -0,0 +1,22 @@
+module Werewolf
+  module Engine
+    class State
+      attr_reader :current, :turn_number, :day_number
+
+      TRANSITIONS = {:dawn => :day, :day => :dusk, :dusk => :night, :night => :dawn}
+
+      # We start at turn 1, :dawn on day 1
+      def initialize
+        @current = :dawn
+        @turn_number, @day_number = 1, 1
+      end
+
+      # Advance to the next time period
+      def next
+        @turn_number += 1
+        @day_number += @turn_number/TRANSITIONS.length
+        @current = TRANSITIONS[@current]
+      end
+    end
+  end
+end
